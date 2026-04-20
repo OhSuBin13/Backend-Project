4f21eda6a52c5d5da0ed55a04124d5a2d6d8bd34
OhSuBin13 <dhtnqls0103@naver.com>
2026-04-20 15:30:48 +0900

    url shortening first init

---
+++ url_shortening/src/main/java/com/example/url_shortening/entity/Link.java
@@ -0,0 +1,61 @@
+package com.example.url_shortening.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = Link.TABLE_NAME, schema = "USS")
+@Entity
+@SequenceGenerator(name = "link_sequence_generator", sequenceName = "link_sequence_generator", allocationSize = 1)
+public class Link implements Serializable {
+
+    public static final String TABLE_NAME = "LINK";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "link_sequence_generator")
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @NotNull
+    @Column(name = "short_code", nullable = false, unique = true)
+    private String shortCode;
+
+    @NotNull
+    @CreatedDate
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private Date createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private Date updatedDate;
+
+    @Column(name = "acces_count")
+    private long accessCount;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdDate = new Date();
+        accessCount = 0;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedDate = new Date();
+        accessCount++;
+    }
+}
