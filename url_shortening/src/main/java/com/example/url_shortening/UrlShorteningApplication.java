93843a6ff93466c6782c5976fe8fcbaf5492141e
OhSuBin13 <dhtnqls0103@naver.com>
2026-04-20 17:59:26 +0900

    DB 테스트 환경 설정

--- url_shortening/src/main/java/com/example/url_shortening/UrlShorteningApplication.java
+++ url_shortening/src/main/java/com/example/url_shortening/UrlShorteningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrlShorteningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShorteningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorteningApplication.class, args);
+    }
 
 }
--- url_shortening/src/test/java/com/example/url_shortening/UrlShorteningApplicationTests.java
+++ url_shortening/src/test/java/com/example/url_shortening/UrlShorteningApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.url_shortening;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class UrlShorteningApplicationTests {
 
 	@Test
